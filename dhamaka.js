30856f242ee945047b349baa50cf782012a752a4
fahim <fahimmontasirshakil@gmail.com>
2021-01-27 20:42:51 +0600

    minor bug fixed

--- dhamaka.js
+++ dhamaka.js
@@ -2,52 +2,50 @@ const iphonePrice = 1200;
 const casePrice = 50;
 //for iphone
 document.getElementById('plusBtn').addEventListener('click', function () {
-    getPlusBtnValue('iphoneAmount','iphoneAmountDoller',iphonePrice);
-   
-    addToCart('iphoneAmountDoller','caseAmountDoller','total','subTotal')
+    getPlusBtnValue('iphoneAmount', 'iphoneAmountdollar', iphonePrice);
+
+    addToCart('iphoneAmountdollar', 'caseAmountdollar', 'total', 'subTotal')
 })
 document.getElementById('minusBtn').addEventListener('click', function () {
-    getMinusBtnValue('iphoneAmount','iphoneAmountDoller',iphonePrice)
-   
-    addToCart('iphoneAmountDoller','caseAmountDoller','total','subTotal')
+    getMinusBtnValue('iphoneAmount', 'iphoneAmountdollar', iphonePrice)
+
+    addToCart('iphoneAmountdollar', 'caseAmountdollar', 'total', 'subTotal')
 })
 //for mobile case 
 document.getElementById('plusBtn2').addEventListener('click', function () {
-    getPlusBtnValue('caseAmount','caseAmountDoller',casePrice);
-   
-    addToCart('iphoneAmountDoller','caseAmountDoller','total','subTotal')
+    getPlusBtnValue('caseAmount', 'caseAmountdollar', casePrice);
+
+    addToCart('iphoneAmountdollar', 'caseAmountdollar', 'total', 'subTotal')
 })
 document.getElementById('minusBtn2').addEventListener('click', function () {
-    getMinusBtnValue('caseAmount','caseAmountDoller',casePrice);
-   
-    addToCart('iphoneAmountDoller','caseAmountDoller','total','subTotal')
+    getMinusBtnValue('caseAmount', 'caseAmountdollar', casePrice);
+
+    addToCart('iphoneAmountdollar', 'caseAmountdollar', 'total', 'subTotal')
 })
 
 
 
 //function for plus button
-function getPlusBtnValue(amount,doller,price) {
+function getPlusBtnValue(amount, dollar, price) {
     const inputNum = parseInt(document.getElementById(amount).value);
-    const textNum = parseInt(document.getElementById(doller).innerText);
-    const currentInputNum = inputNum + 1 ;
-    const currentTextNum = currentInputNum* price;
-    document.getElementById(amount).value =currentInputNum ;
-    document.getElementById(doller).innerText = currentTextNum;
+    const currentInputNum = inputNum + 1;
+    const currentTextNum = currentInputNum * price;
+    document.getElementById(amount).value = currentInputNum;
+    document.getElementById(dollar).innerText = currentTextNum;
 }
 //function for minus button
-function getMinusBtnValue(amount,doller,price) {
+function getMinusBtnValue(amount, dollar, price) {
     const inputNum = parseInt(document.getElementById(amount).value);
-    const textNum = parseInt(document.getElementById(doller).innerText);
-    const currentInputNum = inputNum - 1 ;
-    const currentTextNum = currentInputNum* price;
-    document.getElementById(amount).value =currentInputNum ;
-    document.getElementById(doller).innerText = currentTextNum;
+    const currentInputNum = inputNum - 1;
+    const currentTextNum = currentInputNum * price;
+    document.getElementById(amount).value = currentInputNum;
+    document.getElementById(dollar).innerText = currentTextNum;
 }
 //function for add to cart 
-function addToCart(iphonAmountDoller,caseAmountDoller,total,subTotal) {
-   const iphonePrice = parseInt(document.getElementById(iphonAmountDoller).innerText);
-   const casePrice =  parseInt(document.getElementById(caseAmountDoller).innerText);
-   const totalPrice = iphonePrice + casePrice;
-   document.getElementById(subTotal).innerText = totalPrice;
-   document.getElementById(total).innerText = totalPrice;
+function addToCart(iphoneAmountdollar, caseAmountdollar, total, subTotal) {
+    const iphonePrice = parseInt(document.getElementById(iphoneAmountdollar).innerText);
+    const casePrice = parseInt(document.getElementById(caseAmountdollar).innerText);
+    const totalPrice = iphonePrice + casePrice;
+    document.getElementById(subTotal).innerText = totalPrice;
+    document.getElementById(total).innerText = totalPrice;
 }
